e6ed72d13d6b277f18de63458c49fd75f50ff3bf
Derick de Jesus <derickcjesus@gmail.com>
2021-02-23 17:39:11 -0300

    add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { CompletedChallenger } from '../components/CompletedChallenger';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -8,13 +11,17 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
-          
           <CompletedChallenger />
+          <Countdown />
         </div>
         <div>
 
