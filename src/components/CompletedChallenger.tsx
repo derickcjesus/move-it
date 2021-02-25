1fe56d668493343907e77ccf532cc8c6e4954df6
Derick de Jesus <derickcjesus@gmail.com>
2021-02-25 12:26:49 -0300

    fix: set up challengesCompleted

--- src/components/CompletedChallenger.tsx
+++ src/components/CompletedChallenger.tsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/CompletedChallenger.module.css';
 
 export function CompletedChallenger(){
+    const { challengesCompleted } = useContext(ChallengesContext)
+
     return (
         <div className={styles.completedChallengerContainer}>
-            <span>Desafios Completos</span> <span>5</span>
+            <span>Desafios Completos</span> <span>{challengesCompleted}</span>
         </div>
     );
 }
