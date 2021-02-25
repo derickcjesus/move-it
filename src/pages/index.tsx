2dbb6ba8ea35bdf8a97d0db0903ae3f19ffb0302
Derick de Jesus <derickcjesus@gmail.com>
2021-02-25 09:05:34 -0300

    feat: create challengeBox when active

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
+import Head from 'next/head';
+
 import { CompletedChallenger } from '../components/CompletedChallenger';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
-import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+
 
 
 export default function Home() {
@@ -24,7 +27,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
